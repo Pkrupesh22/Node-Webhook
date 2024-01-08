f6eef2ff0d825e32406ebdcddf333904f7b997ba
Pkrupesh22 <128695536+Pkrupesh22@users.noreply.github.com>
2024-01-09 00:29:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/webhook', (req, res) => {
   }
 })
 
-app.post('/webhook', (req, res) => {
+app.post('/web', (req, res) => {
   let bodyMess = req.body
   if (bodyMess.object) {
     console.log('bodyMess if in :-', JSON.stringify(bodyMess) )
